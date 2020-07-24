392afe61c4d6e6ff815da836e5b4eaf0dc68b6b1
Vitalii Bandyl <vitaliibandyl@gmail.com>
2020-07-24 15:33:58 +0300

    CRUD API with APIView

---
+++ article/views.py
@@ -0,0 +1,39 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+        # Create an article from the above data
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+            return Response({"success": f"Article '{article_saved.title}' created successfully"})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+            return Response({
+                "success": f"Article '{article_saved.title}' updated successfully"
+            })
+
+    def delete(self, request, pk):
+        # Get object with this pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({
+            "message": f"Article with id `{pk}` has been deleted."
+        }, status=204)
+
